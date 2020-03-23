14ff48402e59657d7901295c9781ad9c02081d1c
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2020-03-23 16:07:12 -0400

    Updates

---
+++ precache-manifest.74086f50cbac31faaf6c30d1f2c88ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80c8ebd47259c4e8f5340de8ed88009",
+    "url": "/triviaBattle/index.html"
+  },
+  {
+    "revision": "cee95b7dc7861ff58755",
+    "url": "/triviaBattle/static/css/main.5b4c38ce.chunk.css"
+  },
+  {
+    "revision": "a796b298b5671396f5d7",
+    "url": "/triviaBattle/static/js/2.85b48de6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/triviaBattle/static/js/2.85b48de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee95b7dc7861ff58755",
+    "url": "/triviaBattle/static/js/main.3886f25b.chunk.js"
+  },
+  {
+    "revision": "bc5da67ced9f5407dce3",
+    "url": "/triviaBattle/static/js/runtime-main.e47d359c.js"
+  }
+]);
